85d4916dbe110f8df58b89873d3f3b8f46a5f819
Sümeyye KAYA <sumeyye.kaya@baogrenci.com>
2019-11-06 12:01:16 +0300

    Add project files.

---
+++ tekrar/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace tekrar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrn = new System.Windows.Forms.Button();
+            this.btnEleman = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbElemanlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOrn
+            // 
+            this.btnOrn.Location = new System.Drawing.Point(12, 38);
+            this.btnOrn.Name = "btnOrn";
+            this.btnOrn.Size = new System.Drawing.Size(120, 23);
+            this.btnOrn.TabIndex = 0;
+            this.btnOrn.Text = "Örnek";
+            this.btnOrn.UseVisualStyleBackColor = true;
+            this.btnOrn.Click += new System.EventHandler(this.BtnOrn_Click);
+            // 
+            // btnEleman
+            // 
+            this.btnEleman.Location = new System.Drawing.Point(12, 67);
+            this.btnEleman.Name = "btnEleman";
+            this.btnEleman.Size = new System.Drawing.Size(120, 23);
+            this.btnEleman.TabIndex = 1;
+            this.btnEleman.Text = "Eleman Ekle";
+            this.btnEleman.UseVisualStyleBackColor = true;
+            this.btnEleman.Click += new System.EventHandler(this.BtnEleman_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lbElemanlar
+            // 
+            this.lbElemanlar.FormattingEnabled = true;
+            this.lbElemanlar.Location = new System.Drawing.Point(12, 96);
+            this.lbElemanlar.Name = "lbElemanlar";
+            this.lbElemanlar.Size = new System.Drawing.Size(120, 95);
+            this.lbElemanlar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(151, 210);
+            this.Controls.Add(this.lbElemanlar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnEleman);
+            this.Controls.Add(this.btnOrn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrn;
+        private System.Windows.Forms.Button btnEleman;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox lbElemanlar;
+    }
+}
+
---
+++ tekrar/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekrar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOrn_Click(object sender, EventArgs e)
+        {
+            //içeriğine sizin karar verebileceğiniz bir sayısal dizi oluşturun ve bu dizinin en küçük - en büyük elemanını messagebox ile gösterin
+
+            int[] sayilar = { 98, 96, 99, 17, 92, 94 };
+
+            //MessageBox.Show("Dizideki en büyük sayı: " + sayilar.Max() + "\nDizideki en küçük sayı: " + sayilar.Min());
+
+            Array.Sort(sayilar);
+            MessageBox.Show($"Dizideki en büyük sayı: {sayilar[0]}\nDizideki en küçük sayı: {sayilar[sayilar.Length - 1]}");
+        }
+
+        string[] kayitlar = new string[0];
+        //int adet = 1;
+        //int index = 0;
+
+        private void BtnEleman_Click(object sender, EventArgs e)
+        {
+            //textboxtan girilen veri "kayıtlar" adlı diziye eklensin. her yeni kayıtta dizinin boyutu bir arttırılsın. eleman eklendikten sonra ise aşağıdaki formatta kayıt listboxta gösterilsin 
+            //FORMAT -- eleman - elemaninIndex
+            // Dim value As Integer = CInt(Int((6 * Rnd()) + 1))
+
+            //string veri = textBox1.Text;
+            //Array.Resize(ref kayitlar, adet);
+            //kayitlar[index] = veri;
+            //adet++;
+            //index++;
+            //lbElemanlar.Items.Add($"{veri} - {index}");
+
+
+            Array.Resize(ref kayitlar, kayitlar.Length + 1);
+            kayitlar[kayitlar.Length - 1] = textBox1.Text;
+            lbElemanlar.Items.Add($"{kayitlar[kayitlar.Length - 1]} - {kayitlar.Length - 1}");
+        }
+    }
+}
